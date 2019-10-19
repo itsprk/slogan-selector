1a68db388b939de5742a0e8f378bf2cb5b584a25
itsprk <prk.shine@gmail.com>
2019-10-19 15:55:37 +0530

    Updates

---
+++ precache-manifest.f586544ba5cb0486292113c3c2d7dac7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d1cda98fa82b93588751d199337d23",
+    "url": "/slogan-selector/index.html"
+  },
+  {
+    "revision": "2db7e41c69b5c4c1179c",
+    "url": "/slogan-selector/static/css/main.95bd8815.chunk.css"
+  },
+  {
+    "revision": "c56f695c370a45341086",
+    "url": "/slogan-selector/static/js/2.a7ee7796.chunk.js"
+  },
+  {
+    "revision": "2db7e41c69b5c4c1179c",
+    "url": "/slogan-selector/static/js/main.1b813139.chunk.js"
+  },
+  {
+    "revision": "7fa68373bfebd32869f0",
+    "url": "/slogan-selector/static/js/runtime-main.6096a996.js"
+  },
+  {
+    "revision": "4c5231a6e797c18862190909ab7865eb",
+    "url": "/slogan-selector/static/media/slogan.4c5231a6.png"
+  }
+]);
